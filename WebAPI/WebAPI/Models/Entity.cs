6b0ce6f2fad144441655578df473864c06bd626b
Rodrigo Fernandes Ribeiro <rodrigofr.br@gmail.com>
2020-09-30 01:32:05 -0300

    criacao_models

---
+++ WebAPI/WebAPI/Models/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    //DataCriar e DataUpdate será herdado por qualquer model da aplicação
+    public abstract class Entity
+    {
+        //DateTime permite acessar o atributo .Now que possui como valor hora local
+        public DateTime DataCriar { get; set; }
+        public DateTime DataUpdate { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Tarefa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    //Herdando o comportamento de Entity...
+    public class Tarefa : Entity
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
